a608c3d5ca6ac881483c7da40542c00309e34add
Jacob Lavarnway <lavarnwayj@thomas.edu>
2020-07-01 17:14:20 -0400

    implemented mutation

--- AI 3.0/Breeding/Mutater.cs
+++ AI 3.0/Breeding/Mutater.cs
@@ -13,7 +13,34 @@ namespace AI_3._0.Breeding
 
         public Solution Mutate(Solution solution)
         {
-            //TBI
+            Random rand = new Random();
+            if (rand.NextDouble() <= mutationRate)
+            {
+
+                int mutatePoint1 = rand.Next(0,solution.path.Length);
+                int mutatePoint2 = rand.Next(1, solution.path.Length + 1);
+                string[] mutatedPath = new string[solution.path.Length];
+                string[] reversedPath = solution.path;
+                reversedPath.Reverse();
+
+                for (int iterator =0; iterator < mutatePoint1; iterator++)
+                {
+                    //Fill normally.
+                    mutatedPath[iterator] = solution.path[iterator];
+
+                }
+                for (int iterator = mutatePoint1; iterator < mutatePoint2; iterator++)
+                {
+                    //fill in reverse.
+                    mutatedPath[iterator] = reversedPath[iterator];
+                }
+                for (int iterator = mutatePoint2; iterator < solution.path.Length;) 
+                {
+                    //fill normally.
+                    mutatedPath[iterator] = solution.path[iterator];
+                }
+                solution.path = mutatedPath;
+            }
             return solution;
         }
 
