3d57013e38677852133642745722a99998f35cc9
Jacob Lavarnway <lavarnwayj@thomas.edu>
2020-07-02 17:19:43 -0400

    updated rouletteWheel to select with bias

--- AI 3.0/Breeding/RouletteWheel.cs
+++ AI 3.0/Breeding/RouletteWheel.cs
@@ -13,9 +13,26 @@ namespace AI_3._0.Breeding
 
         public Solution SelectParent()
         {
+            //parent value is a placeholer until for loop finds the real parent.
+            Solution parent = generation[1];
             Random rand = new Random();
-            int i = rand.Next(0, generation.Length);
-            Solution parent = generation[i];
+            double rouletteSelection = rand.NextDouble();
+
+            if (rouletteSelection > generation.Last().rouletteEdge)
+            {
+                parent = generation.Last();
+            }
+            else {
+                for (int x = 0; x < generation.Length - 1; x++)
+                {
+                    //if selection falls between x and x+1's roulette edges, then the object at x has been selected as a parent.
+                    if  (generation[x].rouletteEdge <= rouletteSelection && generation[x + 1].rouletteEdge > rouletteSelection)
+                    {
+                        parent = generation[x];
+                        break;
+                    }
+                }
+            }
             return parent;
         }
 
