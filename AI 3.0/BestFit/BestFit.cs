25dd7e0bebaa4b7bf8646a1d4bff02c86c9939c9
Jacob Lavarnway <lavarnwayj@thomas.edu>
2020-07-03 18:41:42 -0400

    Added class to find best fit object from a provided generation

---
+++ AI 3.0/BestFit/BestFit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AI_3._0.Interfaces;
+using AI_3._0.Data_Classes;
+namespace AI_3._0.BestFit
+{
+    class BestFit : IBestFit
+    {
+        FitnessObject[] bestFitArray;
+        IComparer<FitnessObject> sortByDifference;
+
+        public Solution FindBestFit(Solution[] generation)
+        {
+            bestFitArray = new FitnessObject[generation.Length];
+
+            //set up ends of the bestFitArray, to avoid overflowing.
+            int firstFitness = generation.First().score;
+            bestFitArray[0] = new FitnessObject(0, firstFitness);
+
+           int lastFitness = generation.Last().score - generation[generation.Count() - 2].score;
+            bestFitArray[generation.Count()-1] = new FitnessObject(generation.Length - 1, lastFitness);
+
+            FillBestFitArray(generation);
+            Array.Sort(bestFitArray,sortByDifference);
+
+            int slotOfBestFitObject = bestFitArray.Last().generationSlot;
+            return generation[slotOfBestFitObject];
+
+        }
+
+        private void FillBestFitArray(Solution[] generation)
+        {
+            for(int slot=1; slot <= generation.Length-2; slot++)
+            {
+                int fitness = generation[slot + 1].score - generation[slot].score;
+                bestFitArray[slot] = new FitnessObject(slot, fitness);
+            }
+
+        }
+
+        public BestFit() {
+
+        }
+    }
+}
---
+++ AI 3.0/Data Classes/FitnessObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AI_3._0.BestFit;
+namespace AI_3._0.Data_Classes
+{
+    class FitnessObject
+    {
+        public int generationSlot { get; set; } 
+        public int fitness { get; set; }
+
+        public FitnessObject(int generationSlot, int fitness)
+        {
+            this.generationSlot = generationSlot;
+            this.fitness = fitness;
+        }
+    }
+}
